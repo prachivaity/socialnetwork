b756b23e23b02a92e6a6ed686be0cfc336cb8f1e
prachivaity <vaity.prachi.13it1019@gmail.com>
2016-09-16 19:41:31 +0530

    profileeditpagecomplete

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Chatty\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 use Chatty\Models\User;
 
@@ -26,6 +27,11 @@ class ProfileController extends Controller
 			'last_name'=>'alpha|max:50',
 			'location'=>'max:30',
 			]);
-		dd('okay!!');
+		Auth::user()->update([
+			'first_name'=>$request->input('first_name'),
+			'last_name'=>$request->input('last_name'),
+			'location'=>$request->input('location'),
+			]);
+		return redirect()->route('profile.edit')->with('info','Your profile has been updated now.');
 	}
 }
