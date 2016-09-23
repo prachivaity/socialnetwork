1b4e7bcf984335640601e6847e39413067c92766
prachivaity <vaity.prachi.13it1019@gmail.com>
2016-09-24 00:10:37 +0530

    replies posting and displaying

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -4,6 +4,7 @@ namespace Chatty\Http\Controllers;
 use Auth;
 use Illuminate\Http\Request;
 use Chatty\Models\User;
+use Chatty\Models\Status;
 
 
 
@@ -21,5 +22,27 @@ class StatusController extends Controller
 		return redirect()
 		->route('home')
 		->with('info','Status posted.');
+	}
+
+	public function postReply(Request $request, $statusId){
+		$this->validate($request,[
+		"reply-{$statusId}"=>'required|max:1000',
+			],[
+			'required'=>'The reply body is required.'
+			]);
+
+ 		$status = Status::notReply()->find($statusId);
+
+		if(!$status){
+			return redirect()->route('home');
+		}
+		if(!Auth::user()->isFriendsWith($status->user)&& Auth::user()->id!==
+			$status->user->id){
+			return redirect()->route('home');
+		}
+		$reply = Status::create([
+		'body'=>$request->input("reply-{$statusId}"),])->user()->associate(Auth::user());
+		$status->replies()->save($reply);
+		return redirect()->back();
 	}
 }
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -12,4 +12,11 @@ class Status extends Model
 	public function user(){
         return $this->belongsTo('Chatty\Models\User','user_id');
 } 
+
+public function scopeNotReply($query){
+	return $query->whereNull('parent_id');
+}
+public function replies(){
+	return $this->hasMany('Chatty\Models\Status','parent_id');
+}
 }
